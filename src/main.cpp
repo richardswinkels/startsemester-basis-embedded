354cc05ef9f985edb846ba9cce57dfcec4dd7ae7
Richard Swinkels <github@richardswinkels.nl>
2025-03-07 16:45:05 +0100

    Blink two leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 #include <Arduino.h>
 
 #define LED_RED 4
+#define LED_YELLOW 7
 
 void setup()
 {
-  Serial.begin(9600);
   pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED_RED, HIGH);
   delay(500);
-  Serial.println("Led aan");
   digitalWrite(LED_RED, LOW); 
   delay(500);
-  Serial.println("Led uit");
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(500);
+  digitalWrite(LED_YELLOW, LOW); 
+  delay(500);
 }
