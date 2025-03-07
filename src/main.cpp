7459b211d00eb598320034163dbfce9eec8f6ac0
Richard Swinkels <github@richardswinkels.nl>
2025-03-07 16:53:44 +0100

    Telprogrammaatje

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
 #include <Arduino.h>
 
-#define LED_RED 4
-#define LED_YELLOW 7
+long telling = 0;
 
 void setup()
 {
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  digitalWrite(LED_RED, HIGH);
-  delay(500);
-  digitalWrite(LED_RED, LOW); 
-  delay(500);
-  digitalWrite(LED_YELLOW, HIGH);
-  delay(500);
-  digitalWrite(LED_YELLOW, LOW); 
-  delay(500);
+  Serial.println(telling);
+  telling++;
+  delay(1000);
 }
